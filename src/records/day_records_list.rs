8dfc03bda53917b3ff1a98dad0d28f2f5ff6d9c6
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-01-04 22:30:09 +0100

    Define return type for diet data list manipulations

--- src/records/day_records_list.rs
+++ src/records/day_records_list.rs
@@ -1,21 +1,38 @@
 use crate::records::day_records;
 use chrono::prelude::*;
 use std::collections::BTreeMap;
+use std::error::Error;
 
 pub struct DayRecordsList {
     records: BTreeMap<DateTime<Utc>, day_records::DayRecords>,
 }
 
 impl DayRecordsList {
-    pub fn add(&mut self, day_records: day_records::DayRecords, date: Option<DateTime<Utc>>) {
+    pub fn new() -> DayRecordsList {
         todo!()
     }
 
-    pub fn remove(&mut self, day_records: day_records::DayRecords, date: Option<DateTime<Utc>>) {
+    pub fn add(
+        &mut self,
+        day_records: day_records::DayRecords,
+        date: Option<DateTime<Utc>>,
+    ) -> Result<day_records::DayRecords, Box<dyn Error>> {
         todo!()
     }
 
-    pub fn range(&self, from: Option<DateTime<Utc>>, to: Option<DateTime<Utc>>) {
+    pub fn remove(
+        &mut self,
+        day_records: day_records::DayRecords,
+        date: Option<DateTime<Utc>>,
+    ) -> Result<day_records::DayRecords, Box<dyn Error>> {
+        todo!()
+    }
+
+    pub fn range(
+        &self,
+        from: Option<DateTime<Utc>>,
+        to: Option<DateTime<Utc>>,
+    ) -> Result<BTreeMap<DateTime<Utc>, day_records::DayRecords>, Box<dyn Error>> {
         todo!()
     }
 }
