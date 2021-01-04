e7410907396190c7e108055db64db675d8889f54
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-01-04 22:21:20 +0100

    Create API methods skeleton to use record and use the diet data

--- src/records/day_records_list.rs
+++ src/records/day_records_list.rs
@@ -2,8 +2,30 @@ use crate::records::day_records;
 use chrono::prelude::*;
 use std::collections::BTreeMap;
 
-struct DayRecordsList {
+pub struct DayRecordsList {
     records: BTreeMap<DateTime<Utc>, day_records::DayRecords>,
+}
+
+impl DayRecordsList {
+    pub fn add(&mut self, day_records: day_records::DayRecords, date: Option<DateTime<Utc>>) {
+        todo!()
+    }
+
+    pub fn remove(&mut self, day_records: day_records::DayRecords, date: Option<DateTime<Utc>>) {
+        todo!()
+    }
+
+    pub fn range(&self, from: Option<DateTime<Utc>>, to: Option<DateTime<Utc>>) {
+        todo!()
+    }
+}
+
+impl Iterator for DayRecordsList {
+    type Item = day_records::DayRecords;
+
+    fn next(&mut self) -> Option<day_records::DayRecords> {
+        todo!()
+    }
 }
 
 #[cfg(test)]
