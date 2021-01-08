bae800dbf2ce8a7e5b43d4aead4fbb9753648141
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-01-08 21:38:23 +0100

    Write tests to cover near all attended add() behaviour.

--- src/records/day_records.rs
+++ src/records/day_records.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct DayRecords {
     pub weight: Option<f32>,
     pub notes: Option<String>,
--- src/records/day_records_list.rs
+++ src/records/day_records_list.rs
@@ -59,6 +59,165 @@ mod tests {
             notes: None,
         };
 
-        drl.add(dr, None)
+        drl.add(dr.clone(), None);
+
+        assert_eq!(*drl.records.get(&Utc::now().date()).unwrap(), dr);
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_today_day_record_specifying_current_date() -> Result<(), Box<dyn Error>> {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: Some(85.0),
+            notes: None,
+        };
+        let date = Utc::now().date();
+
+        drl.add(dr.clone(), Some(date.clone()));
+
+        assert_eq!(*drl.records.get(&Utc::now().date()).unwrap(), dr);
+        // TODO: check how to retrieve first key of BTreeMap,
+        // but Mom is calling for dinner.
+        Ok(())
+    }
+
+    #[test]
+    fn add_someday_day_record() -> Result<(), Box<dyn Error>> {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: Some(85.0),
+            notes: None,
+        };
+        let date = Utc.ymd(1994, 05, 18);
+
+        drl.add(dr.clone(), Some(date));
+
+        assert_eq!(*drl.records.get(&Utc::now().date()).unwrap(), dr);
+
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_future_day_record() {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: Some(85.0),
+            notes: None,
+        };
+        let date = Utc.ymd(3994, 05, 18);
+
+        drl.add(dr, Some(date)).unwrap()
+    }
+
+    #[test]
+    fn add_today_day_record_twice() -> Result<(), Box<dyn Error>> {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr_first = DayRecords {
+            weight: Some(85.0),
+            notes: None,
+        };
+        let dr_second = DayRecords {
+            weight: Some(80.0),
+            notes: None,
+        };
+
+        drl.add(dr_first, None);
+        drl.add(dr_second.clone(), None);
+
+        assert_eq!(*drl.records.get(&Utc::now().date()).unwrap(), dr_second);
+
+        Ok(())
+    }
+
+    #[test]
+    fn add_someday_day_record_twice() -> Result<(), Box<dyn Error>> {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr_first = DayRecords {
+            weight: Some(85.0),
+            notes: None,
+        };
+        let dr_second = DayRecords {
+            weight: Some(80.0),
+            notes: None,
+        };
+        let date = Utc.ymd(1994, 05, 18);
+
+        drl.add(dr_first, Some(date));
+        drl.add(dr_second.clone(), Some(date));
+
+        assert_eq!(*drl.records.get(&Utc::now().date()).unwrap(), dr_second);
+
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_today_day_record_negative_weight() {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: Some(-85.0),
+            notes: None,
+        };
+
+        drl.add(dr, None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_someday_day_record_negative_weight() {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: Some(-85.0),
+            notes: None,
+        };
+        let date = Utc.ymd(1994, 05, 18);
+
+        drl.add(dr, Some(date));
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_today_empty_day_record() {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: None,
+            notes: None,
+        };
+
+        drl.add(dr, None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_someday_empty_day_record() {
+        let mut drl = DayRecordsList {
+            records: BTreeMap::new(),
+        };
+        let dr = DayRecords {
+            weight: None,
+            notes: None,
+        };
+        let date = Utc.ymd(1994, 05, 18);
+
+        drl.add(dr, Some(date));
     }
 }
