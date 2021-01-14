b0d16e423627da008f61d53e6b042ac1ee707237
François de Mareschal <francois.de-mareschal@outlook.fr>
2021-01-15 00:44:36 +0100

    Implement RecordingsError in top module

--- src/recordings/mod.rs
+++ src/recordings/mod.rs
@@ -1,3 +1,38 @@
 mod day_records;
 mod day_records_list;
 mod report;
+
+mod error {
+    use chrono::prelude::*;
+    use std::error::Error;
+    use std::fmt::{self, Display};
+
+    #[derive(Debug)]
+    enum RecordingsError {
+        DateInTheFuture(Date<Utc>),
+        DayRecordsEmpty,
+        NoDayRecordsAtDate(Date<Utc>),
+        StartDateAfterEndDate {
+            start_date: Date<Utc>,
+            end_date: Date<Utc>,
+        },
+        ZeroOrNegativeWeight(f32),
+    }
+
+    impl Display for RecordingsError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match &self {
+                RecordingsError::DateInTheFuture(date) => todo!(),
+                RecordingsError::DayRecordsEmpty => todo!(),
+                RecordingsError::NoDayRecordsAtDate(date) => todo!(),
+                RecordingsError::StartDateAfterEndDate {
+                    start_date,
+                    end_date,
+                } => todo!(),
+                RecordingsError::ZeroOrNegativeWeight(weight) => todo!(),
+            }
+        }
+    }
+
+    impl Error for RecordingsError {}
+}
